82db3e503691312541292b70cf7798762fb633a0
SAlfarisi <ipmawansalman@gmail.com>
2025-12-02 05:35:10 +0700

    Calender v1

--- assets/js/main.js
+++ assets/js/main.js
@@ -185,3 +185,57 @@
   document.addEventListener('scroll', navmenuScrollspy);
 
 })();
+
+const MonthYearElement = document.getElementById('monthYear');
+const datesElement = document.getElementById('dates');
+const prevBtn = document.getElementById('prevBtn');
+const nextBtn = document.getElementById('nextBtn');
+
+let currenDate = new Date();
+
+const UpdateCalendar = () => {
+  const currentYear = currenDate.getFullYear();
+  const currentMonth = currenDate.getMonth();
+
+  const firstDay = new Date(currentYear. currentMonth, 0);
+  const lastDay = new Date(currentYear, currentMonth + 1, 0);
+  const totalDays = lastDay.getDate();
+  const firstDayIndex = firstDay.getDay();
+  const lastDayIndex = lastDay.getDay();
+
+  const monthYearString = currenDate.toLocaleString
+  ('default', {month: 'long', year: 'numeric'});
+  MonthYearElement.textContent = monthYearString;
+
+  let datesHTML = '';
+
+  for(let i = firstDayIndex; i > 0; i--) {
+    const prevDate = new Date(currentYear,currentMonth, 0 - i +1);
+    datesHTML += `<div class="date inactive">${prevDate.getDate()}</div>`;
+  }
+
+  for (let i = 1; i <= totalDays; i++) {
+    const date = new Date(currentYear, currentMonth, i);
+    const activeClass = date.toDateString() === new Date().toDateString() ? 'active' : '';
+    datesHTML += `<div class="date ${activeClass}">${i}</div>`;
+  }
+
+  for (let i = 1; i <= 7 - lastDayIndex; i++) {
+    const nextDate = new Date(currentYear, currentMonth + 1, i);
+    datesHTML += `<div class="date inactive">${nextDate.getDate()}</div>`;
+  }
+
+  datesElement.innerHTML = datesHTML;
+}
+
+prevBtn.addEventListener('click', () => {
+  currenDate.setMonth(currenDate.getMonth() - 1);
+  UpdateCalendar();
+})
+
+nextBtn.addEventListener('click', () => {
+  currenDate.setMonth(currenDate.getMonth() + 1);
+  UpdateCalendar();
+})
+
+UpdateCalendar();
